ddfe7dd59bb3b404d8c7227ab608b42f934d54b0
oAura <albert.khachatryan.05@gmail.com>
2021-11-26 17:10:56 +0400

    initial commit

--- data/src/main/java/aura/projects/data/util/AuthenticationInterceptor.kt
+++ data/src/main/java/aura/projects/data/util/AuthenticationInterceptor.kt
@@ -4,6 +4,13 @@ import okhttp3.*
 
 class AuthenticationInterceptor: Interceptor {
     private val AUTHORIZATION = "Authorization"
+    private val ACCEPT_LANGUAGE = "Accept-Language"
+    private val CONTENT_TYPE = "Content-Type"
     private val TOKEN = "Basic bW9iaWxlY2xpZW50OkxXdzhudzRjdlNvN0tkQlNWZ0Fq"
-    override fun intercept(chain: Interceptor.Chain): Response = chain.proceed(chain.request().newBuilder().addHeader(AUTHORIZATION, TOKEN).build())
+    override fun intercept(chain: Interceptor.Chain): Response =
+        chain.proceed(chain.request().newBuilder()
+            .addHeader(AUTHORIZATION, TOKEN)
+            .addHeader(ACCEPT_LANGUAGE, "ru_RU")
+            .addHeader(CONTENT_TYPE, "application/json")
+            .build())
 }
